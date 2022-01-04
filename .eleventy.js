931e7cfff105e89ae871357b28b8474155d2a119
Jason Carrasco <5018598+jtcarrasco@users.noreply.github.com>
2022-01-04 09:06:56 -0800

    20220104 initial commit

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+const { DateTime } = require("luxon");
+
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy('./src/style.css'); //styling in public folder
+  eleventyConfig.addPassthroughCopy('./src/assets'); // assets in public folder
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "public"
+    }
+  };
+}
