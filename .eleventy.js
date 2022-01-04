4bfbd7159b5a106aa57b759f6e5704c7b56f3fad
Jason Carrasco <5018598+jtcarrasco@users.noreply.github.com>
2022-01-04 09:37:51 -0800

    fixed admin panel

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('./src/style.css'); //styling in public folder
   eleventyConfig.addPassthroughCopy('./src/assets'); // assets in public folder
+  eleventyConfig.addPassthroughCopy('./src/admin'); // assets in public folder
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
